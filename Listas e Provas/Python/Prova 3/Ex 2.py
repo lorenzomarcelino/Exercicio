4e76066cdb09cc77fae45d82dabbed1f7f80ee4e
Lorenzo Marcelino <lorenzommarcelino@gmail.com>
2022-06-14 15:57:13 -0300

    Create Ex 2.py

---
+++ Listas e Provas/Python/Prova 3/Ex 2.py
@@ -0,0 +1,7 @@
+# (Dicionários e String – 30 pontos) Uma turma foi dividida em 3 grupos com 3
+# pessoas cada. Escreva um programa que lê o nome do grupo e as alturas das
+# três pessoas de cada grupo. Essas alturas são armazenadas em um dicionário,
+# onde a chave é o nome do grupo. Por fim, deverá ser mostrado um dicionário,
+# onde a chave é o nome do grupo e o valor é a média de alturas (com duas casas
+# decimais). Também deve ser apresentado o nome do grupo que obteve a maior
+# média de alturas (com a primeira letra maiúscula).
